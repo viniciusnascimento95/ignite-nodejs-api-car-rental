87fd749fa1855c9bb8e4b43098cfd0945a8cdbf6
vinicius <nascimento.vinicius32@gmail.com>
2022-03-22 20:46:10 -0300

    update config multer

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
 
+import multer from "multer";
+
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
@@ -12,4 +14,7 @@ categoriesRoutes.post("/", (request, response) =>
 categoriesRoutes.get("/", (request, response) =>
     listCategoriesController.handle(request, response)
 );
+
+categoriesRoutes.post("/import", (request, response) => {});
+
 export { categoriesRoutes };
