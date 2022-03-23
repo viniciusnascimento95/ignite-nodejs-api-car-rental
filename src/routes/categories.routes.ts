1456b8a827249a224ba4d0f5b11b666808aa2782
vinicius <nascimento.vinicius32@gmail.com>
2022-03-23 00:33:46 -0300

    config upload with multer

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
-
 import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { importCategoryController } from "../modules/cars/useCases/importCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
+
+const upload = multer({
+    dest: "./tmp",
+});
 
 const categoriesRoutes = Router();
 
@@ -15,6 +19,8 @@ categoriesRoutes.get("/", (request, response) =>
     listCategoriesController.handle(request, response)
 );
 
-categoriesRoutes.post("/import", (request, response) => {});
+categoriesRoutes.post("/import", upload.single("file"), (request, response) =>
+    importCategoryController.handle(request, response)
+);
 
 export { categoriesRoutes };
