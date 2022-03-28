f35fa479c6928ad632b7f2edb2df91777b98f792
vinicius <nascimento.vinicius32@gmail.com>
2022-03-28 17:38:21 -0300

    create migrations specifications and config

--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -1,3 +1,5 @@
+import { getRepository, Repository } from "typeorm";
+
 import { Specification } from "../../entities/Specification";
 import {
     ICreateSpecificationDTO,
@@ -5,28 +7,38 @@ import {
 } from "../ISpecificationsRepository";
 
 class SpecificationsRepository implements ISpecificationsRepository {
-    private specifications: Specification[];
+    // private specifications: Specification[];
+
+    private repository: Repository<Specification>;
 
     constructor() {
-        this.specifications = [];
+        // this.specifications = [];
+        this.repository = getRepository(Specification);
     }
 
-    create({ name, description }: ICreateSpecificationDTO): void {
-        const specification = new Specification();
+    async create({
+        name,
+        description,
+    }: ICreateSpecificationDTO): Promise<void> {
+        const specification = this.repository.create({ name, description });
 
-        Object.assign(specification, {
-            name,
-            description,
-            created_at: new Date(),
-        });
+        await this.repository.save(specification);
 
-        this.specifications.push(specification);
+        // const specification = new Specification();
+        // Object.assign(specification, {
+        //     name,
+        //     description,
+        //     created_at: new Date(),
+        // });
+        // this.specifications.push(specification);
     }
 
-    findByName(name: string): Specification {
-        const specification = this.specifications.find(
-            (specification) => specification.name === name
-        );
+    async findByName(name: string): Promise<Specification> {
+        // const specification = this.specifications.find(
+        //     (specification) => specification.name === name
+        // );
+        const specification = this.repository.findOne({ name });
+
         return specification;
     }
 }
--- src/modules/cars/useCases/createSpecification/CreateSpecificationController.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationController.ts
@@ -4,18 +4,14 @@ import { container } from "tsyringe";
 import { CreateSpecificationUseCase } from "./CreateSpecificationUseCase";
 
 class CreateSpecificationController {
-    // constructor(
-    //     private createSpecificationUseCase: CreateSpecificationUseCase
-    // ) {}
-
-    handle(resquest: Request, response: Response) {
+    async handle(resquest: Request, response: Response): Promise<Response> {
         const { name, description } = resquest.body;
 
         const createSpecificationUseCase = container.resolve(
             CreateSpecificationUseCase
         );
 
-        createSpecificationUseCase.execute({ name, description });
+        await createSpecificationUseCase.execute({ name, description });
 
         return response.status(201).send();
     }
