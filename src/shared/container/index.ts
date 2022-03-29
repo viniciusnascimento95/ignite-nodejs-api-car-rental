e20ba29ae86547c770b7fe3ba9b78e96d306da7b
vinicius <nascimento.vinicius32@gmail.com>
2022-03-29 00:36:10 -0300

    update docs swagger

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,4 +1,5 @@
 import { container } from "tsyringe";
+// import "reflect-metadata";
 
 import { ICategoriesRepository } from "../../modules/cars/repositories/ICategoriesRepository";
 import { CategoriesRepository } from "../../modules/cars/repositories/implementations/CategoriesRepository";
