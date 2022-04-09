2cbc6b0d706fc70df8d66a5e5369ee1f67570046
Vinicius <vinicius.souza@smartsupport.com.br>
2022-04-09 18:24:28 -0300

    feat update errors import

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,13 +1,14 @@
 import { container } from "tsyringe";
 
-import { UsersRepository } from "../../modules/accounts/repositories/implementations/UsersRepository";
-import { IUsersRepository } from "../../modules/accounts/repositories/IUsersRepository";
+import { UsersRepository } from "@modules/accounts/repositories/implementations/UsersRepository";
+// eslint-disable-next-line import-helpers/order-imports
+import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
 // import "reflect-metadata";
 
-import { ICategoriesRepository } from "../../modules/cars/repositories/ICategoriesRepository";
-import { CategoriesRepository } from "../../modules/cars/repositories/implementations/CategoriesRepository";
-import { SpecificationsRepository } from "../../modules/cars/repositories/implementations/SpecificationsRepository";
-import { ISpecificationsRepository } from "../../modules/cars/repositories/ISpecificationsRepository";
+import { ICategoriesRepository } from "@modules/cars/repositories/ICategoriesRepository";
+import { CategoriesRepository } from "@modules/cars/repositories/implementations/CategoriesRepository";
+import { SpecificationsRepository } from "@modules/cars/repositories/implementations/SpecificationsRepository";
+import { ISpecificationsRepository } from "@modules/cars/repositories/ISpecificationsRepository";
 
 container.registerSingleton<ICategoriesRepository>(
     "CategoriesRepository",
