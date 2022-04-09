87a3dcf8a9dac208bf42acfb18e06abc1aafc840
vinicius <nascimento.vinicius32@gmail.com>
2022-04-09 13:29:45 -0300

    update first test

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -20,7 +20,7 @@ class CreateCategoryUseCase {
             await this.categoriesRepository.findByName(name);
 
         if (categoryAlreadyExists) {
-            throw new AppError("Category already exists");
+            throw new AppError("Category already exists", 401);
         }
 
         this.categoriesRepository.create({ name, description });
