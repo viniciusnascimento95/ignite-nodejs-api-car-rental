d2bcc21e63e7fe35d3809778f950225fa89c3ee8
Vinicius <vinicius.souza@smartsupport.com.br>
2022-04-12 08:37:35 -0300

    config cars repository

---
+++ src/modules/cars/useCases/createCar/CreateCarController.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express";
+
+class CreateCarController {
+    async handle(request: Request, response: Response): Promise<Response> {
+        // const
+    }
+}
+
+export { CreateCarController };
