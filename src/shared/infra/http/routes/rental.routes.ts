ee584a6200c2c12da7da0bfb335d145a3eb21843
vinicius <nascimento.vinicius32@gmail.com>
2022-08-04 18:50:13 -0300

    list users cars rentals

--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -1,17 +1,25 @@
 import { CreateRentalController } from "@modules/rentals/useCases/createRental/CreateRentalController";
 import { DevolutionRentalController } from "@modules/rentals/useCases/devolutionRental/DevolutionRentalController";
+import { ListRentalsByUserUseController } from "@modules/rentals/useCases/listRentalsByUser/ListRentalsByUserController";
 import { Router } from "express";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 const rentalRoutes = Router();
 
 const createRentalController = new CreateRentalController();
 const devolutionRentalController = new DevolutionRentalController();
+const listRentalsByUserController = new ListRentalsByUserUseController();
 
 rentalRoutes.post("/", ensureAuthenticated, createRentalController.handle);
 rentalRoutes.post(
     "/devolution/:id",
     ensureAuthenticated,
     devolutionRentalController.handle
+);
+
+rentalRoutes.get(
+    "/user",
+    ensureAuthenticated,
+    listRentalsByUserController.handle
 );
 
 export { rentalRoutes };
