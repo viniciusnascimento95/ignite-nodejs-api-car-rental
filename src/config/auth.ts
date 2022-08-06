5528f9fe5b142ee30d03b1d99fe2619daa22b34b
vinicius <nascimento.vinicius32@gmail.com>
2022-08-06 18:59:26 -0300

    refactor auth user

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+    secret_token: "941c421d551126ace7478d099cddfdb7",
+    expires_in_token: "15m",
+    secret_refresh_token: "752eb0144f43e4a1542f33f0a8755fce",
+    expires_in_refresh_token: "30d",
+    expires_refresh_token_days: 30,
+};
